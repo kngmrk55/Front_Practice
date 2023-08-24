dcf8cfabedde1318bd75b6b2d417eecd919ab615
DESKTOP-830SSS8\MarcoPerez <perez_marco98@hotmail.com>
2023-08-24 14:43:38 -0600

    Day 8 and Day 9 Projects

---
+++ 8.Form_Input_Wave/js/script.js
@@ -0,0 +1,8 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(labels => {
+    labels.innerHTML = labels.innerHTML
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('');
+});
